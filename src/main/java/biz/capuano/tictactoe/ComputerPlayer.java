e5bf57f0bd8f811d860967215355fc0570e8844e
niccap <nicola@capuano.biz>
2023-10-14 19:07:55 +0200

    Initial Version

--- src/main/java/biz/capuano/tictactoe/ComputerPlayer.java
+++ src/main/java/biz/capuano/tictactoe/ComputerPlayer.java
@@ -12,7 +12,7 @@ class ComputerPlayer extends Player {
     public Move makeMove(Board board) {
         Move move;
         do {
-            move = new Move(random.nextInt(board.SIZE) + 1, random.nextInt(board.SIZE) + 1);
+            move = new Move(random.nextInt(Board.SIZE) + 1, random.nextInt(Board.SIZE) + 1);
         } while (!board.makeMove(move, mark));
         System.out.println("Computer player " + mark + ", moves in row " + move.getRow() + ", column " + move.getCol());
         return move;
