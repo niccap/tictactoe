4554f9d13783cb5fa8c15d444e7a45a400fa3ab3
niccap <nicola@capuano.biz>
2023-11-06 14:42:27 +0100

    Initial Version

---
+++ src/test/java/biz/capuano/tictactoe/MoveTest.java
@@ -0,0 +1,51 @@
+package biz.capuano.tictactoe;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MoveTest {
+    
+    public MoveTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getRow method, of class Move.
+     */
+    @Test
+    public void testGetRow() {
+        System.out.println("getRow");
+        Move instance = new Move(1, 2);
+        int expResult = 1;
+        int result = instance.getRow();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getCol method, of class Move.
+     */
+    @Test
+    public void testGetCol() {
+        System.out.println("getCol");
+        Move instance = new Move(1, 2);
+        int expResult = 2;
+        int result = instance.getCol();
+        assertEquals(expResult, result);
+    }
+    
+}
