9918d9c77d620e83c471173b0dbd0f94e52fa76f
niccap <nicola@capuano.biz>
2023-11-06 22:45:26 +0100

    Initial Version

--- src/test/java/biz/capuano/tictactoe/HumanPlayerTest.java
+++ src/test/java/biz/capuano/tictactoe/HumanPlayerTest.java
@@ -2,11 +2,14 @@ package biz.capuano.tictactoe;
 
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
+import java.io.*;
+
 
 public class HumanPlayerTest {
     
-    HumanPlayer player;
-    
+    private HumanPlayer player;
+    private Board board;
+
     public HumanPlayerTest() {
     }
     
@@ -21,6 +24,7 @@ public class HumanPlayerTest {
     @BeforeEach
     public void setUp() {
         player = new HumanPlayer('O');
+        board = new Board();
     }
     
     @AfterEach
@@ -39,9 +43,52 @@ public class HumanPlayerTest {
     /**
      * Test of makeMove method, of class HumanPlayer.
      */
+        
     @Test
-    public void testMakeMove() {
-        System.out.println("Tesing MakeMove: Needs Manual Testing");
+    public void testMakeMove1() {
+        System.out.println("Tesing MakeMove: valid move");
+
+        simulateInput("2 3\n");
+        Move move = player.makeMove(board);
+        assertEquals(2, move.getRow());
+        assertEquals(3, move.getCol());
+        restoreInput();
+    }    
+    @Test
+    public void testMakeMove2() {
+        System.out.println("Tesing MakeMove: invalid move");
+        
+        simulateInput("1 4 2 3\n");
+        Move move = player.makeMove(board);
+        assertEquals(2, move.getRow());
+        assertEquals(3, move.getCol());
+        restoreInput();
+    }    
+    @Test
+    public void testMakeMove3() {
+        System.out.println("Tesing MakeMove: occupied cell");
+        
+        simulateInput("2 3\n");
+        player.makeMove(board);
+        simulateInput("2 3 3 3\n");
+        Move move = player.makeMove(board);
+        assertEquals(3, move.getRow());
+        assertEquals(3, move.getCol());
+        restoreInput();
     }    
 
+    /**
+     * Helper fuctions to simulate user input
+     */
+
+    private InputStream originalInput; 
+
+    private void simulateInput(String in) {
+        originalInput = System.in; // Save the current input stream 
+        System.setIn(new ByteArrayInputStream(in.getBytes())); // Temp input stream        
+    }
+    
+    private void restoreInput() {
+        System.setIn(originalInput); 
+    }
 }
